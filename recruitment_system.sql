46610086b0f16cb63fb5defa3b7d6fdeb5f6dd60
Developer Majesty <aidoojuniorsolomon@gmail.com>
2021-01-12 22:58:41 +0000

    Commit Database

--- recruitment_system.sql
+++ recruitment_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 23, 2020 at 05:41 PM
+-- Generation Time: Jan 12, 2021 at 11:55 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -46,7 +46,8 @@ INSERT INTO `admin` (`id`, `username`, `password`, `email`) VALUES
 (4, 'adminpeter', 'peter', 'aidoojuniorsolomon@yahoo.com'),
 (5, 'adminpeter', 'peter', 'aidoojuniorsolomon@yahoo.com'),
 (6, 'adminpeter', 'peter', 'aidoojuniorsolomon@yahoo.com'),
-(7, 'adminruth', 'ruth', 'aidoojuniorsolomon@yahoo.com');
+(7, 'adminruth', 'ruth', 'aidoojuniorsolomon@yahoo.com'),
+(8, 'tester', 'majestymajesty', 'majesty@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -100,14 +101,13 @@ CREATE TABLE `banned_users` (
 --
 
 INSERT INTO `banned_users` (`id`, `username`, `password`, `email`, `image`) VALUES
-(1, 'sandy', 'sandy', 'sandy@gmail.com', ''),
-(2, 'sam', 'sam', 'aidoojuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
+(1, 'sam', 'sam', 'aidoojuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
+(2, 'solo', 'solo', 'aidoohuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
 (3, 'solo', 'solo', 'aidoohuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
 (4, 'solo', 'solo', 'aidoohuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
-(5, 'solo', 'solo', 'aidoohuniorsolomon@gmail.com', 'profile_pics/FB_IMG_1522653386797-01.jpeg'),
-(6, 'junior', 'junior', 'aidoojuniorsolomon@yahoo.com', 'profile_pics/RoundPhoto_Feb152018_132430-01.jpeg'),
-(7, 'dave', 'dave', 'aidoohuniorsolomon@gmail.com', 'profile_pics/RoundPhoto_Jan272018_083220-01.jpeg'),
-(8, 'sandy', 'sandy', 'sandy@gmail.com', '');
+(5, 'junior', 'junior', 'aidoojuniorsolomon@yahoo.com', 'profile_pics/RoundPhoto_Feb152018_132430-01.jpeg'),
+(6, 'dave', 'dave', 'aidoohuniorsolomon@gmail.com', 'profile_pics/RoundPhoto_Jan272018_083220-01.jpeg'),
+(7, 'sandy', 'sandy', 'sandy@gmail.com', '');
 
 -- --------------------------------------------------------
 
@@ -133,12 +133,11 @@ INSERT INTO `jobs` (`id`, `job_title`, `vacancy`, `min_qualification`, `salary`,
 (2, 'DevsOP Engineer', 4, 'BSc Computer Science / Related field', 6000, 'Must have at least 1 year experience.'),
 (3, 'Android Developer', 2, 'BSc Computer Science ', 5500, 'Must be conversant with Android Studio'),
 (4, 'Database Adminstrator', 5, 'BSc Computer Science ', 4000, 'Must be conversant with oracle DB'),
-(5, 'AI Engineer', 7, 'MSc Computer Science', 20000, 'Must have at least 2 years working experience.'),
-(6, 'PHP Developer', 3, 'BSc ICT / Related field', 7000, 'At least 2 years working experience.'),
-(7, 'Systems Analyst', 2, 'MSc Computer Science', 56000, 'Be posses all the characteristics of a good systems analyst.'),
-(8, 'Chartered Accountant', 3, 'BBA Accounting', 6000, 'Must be have at least 3 years working experience'),
-(9, 'Network Architect', 3, 'MSc ICT', 5600, 'Must have at least 2 years working experience'),
-(10, 'Business Analyst', 4, 'MBA Business Administration', 7000, 'Must be innovative enough to monetize technology. ');
+(5, 'PHP Developer', 3, 'BSc ICT / Related field', 7000, 'At least 2 years working experience.'),
+(6, 'Systems Analyst', 2, 'MSc Computer Science', 56000, 'Be posses all the characteristics of a good systems analyst.'),
+(7, 'Chartered Accountant', 3, 'BBA Accounting', 6000, 'Must be have at least 3 years working experience'),
+(8, 'Network Architect', 3, 'MSc ICT', 5600, 'Must have at least 2 years working experience'),
+(9, 'Business Analyst', 4, 'MBA Business Administration', 7000, 'Must be innovative enough to monetize technology. ');
 
 -- --------------------------------------------------------
 
@@ -165,9 +164,10 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `image`) VALUES
 (4, 'junior', 'junior', 'aidoojuniorsolomon@yahoo.com', '..profile_pics/RoundPhoto_Feb152018_132430-01.jpeg'),
 (5, 'dave', 'dave', 'aidoohuniorsolomon@gmail.com', '../profile_pics/RoundPhoto_Jan272018_083220-01.jpeg'),
 (6, 'sandy', 'sandy', 'sandy@gmail.com', ''),
-(7, 'atsu', 'aaa', 'atsuraymond88@yahoo.com', '../profile_pics/â€ª+233 57 650 3947â€¬ 20180202_221013-01.jpeg'),
-(8, 'developer1', 'Majesty1@', 'aidoojuniorsolomon@yahoo.com', '../profile_pics/IMG_20171115_100223_259-01.jpeg'),
-(9, 'peter', 'peter', 'aidoojuniorsolomon@yahoo.com', '../profile_pics/Complementary Card.jpg');
+(7, 'developer1', 'Majesty1@', 'aidoojuniorsolomon@yahoo.com', '../profile_pics/IMG_20171115_100223_259-01.jpeg'),
+(8, 'peter', 'peter', 'aidoojuniorsolomon@yahoo.com', '../profile_pics/Complementary Card.jpg'),
+(9, 'atsu', 'aaa', 'atsuraymond88@yahoo.com', '../profile_pics/â€ª+233 57 650 3947â€¬ 20180202_221013-01.jpeg'),
+(10, 'sandy', 'sandy', 'sandy@gmail.com', '');
 
 --
 -- Indexes for dumped tables
@@ -211,7 +211,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `applications`
@@ -223,19 +223,19 @@ ALTER TABLE `applications`
 -- AUTO_INCREMENT for table `banned_users`
 --
 ALTER TABLE `banned_users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
